9165f77943698c285da1ff8beca1b30c93db84a0
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-17 23:17:49 +0700

    same code with perf_counter function

--- mf_01_time.py
+++ mf_01_time.py
@@ -13,7 +13,8 @@ import time
 # print('Month: ', time_here[1], time_here.tm_mon)    # second way is easier to read
 # print('Day: ', time_here[2], time_here.tm_mday)
 
-from time import time as my_timer
+# from time import time as my_timer         # cheat on pressing double ENTER
+from time import perf_counter as my_timer
 import random
 
 input('Press ENTER to start')
@@ -25,6 +26,6 @@ start_time = my_timer()
 input('Press ENTER to stop')
 end_time = my_timer()
 
-print('Starterd at: ' + time.strftime("%X", time.localtime(start_time)))
+print('Started at: ' + time.strftime("%X", time.localtime(start_time)))
 print('Stopped at: ' + time.strftime("%X", time.localtime(end_time)))
 print(f'Your reaction time was {end_time - start_time} seconds')
