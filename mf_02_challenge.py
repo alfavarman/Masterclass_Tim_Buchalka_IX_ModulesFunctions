68c9a3dc03e74c77c3ae140456e1270c8a509557
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-17 23:46:40 +0700

    idiot challenge...

--- mf_02_challenge.py
+++ mf_02_challenge.py
@@ -4,3 +4,9 @@
 #
 # Use the documentation for the get_clock_info() function
 # to work out how to call it for each of the clocks.
+import time
+
+print('time: \t\t\t', time.get_clock_info('time'))
+print('perf_counter: \t\t\t', time.get_clock_info('perf_counter'))
+print('monotonic: \t\t\t', time.get_clock_info('monotonic'))
+print('process_time: \t\t\t', time.get_clock_info('process_time'))
