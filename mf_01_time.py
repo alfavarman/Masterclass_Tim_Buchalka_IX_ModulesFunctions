889ebe23c0385d123c748dc32d454c218774b335
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-17 23:03:03 +0700

    reaction time using time

--- mf_01_time.py
+++ mf_01_time.py
@@ -6,8 +6,25 @@ import time
 #
 # print(time.time())
 
-time_here = time.localtime()
-print(time_here)
-print('Year: ', time_here[0], time_here.tm_year)    # two ways to, code is equal - no practical diff
-print('Month: ', time_here[1], time_here.tm_mon)    # second way is easier to read
-print('Day: ', time_here[2], time_here.tm_mday)
+# Date:
+# time_here = time.localtime()
+# print(time_here)
+# print('Year: ', time_here[0], time_here.tm_year)    # two ways to, code is equal - no practical diff
+# print('Month: ', time_here[1], time_here.tm_mon)    # second way is easier to read
+# print('Day: ', time_here[2], time_here.tm_mday)
+
+from time import time as my_timer
+import random
+
+input('Press ENTER to start')
+
+
+wait_time = random.randint(1, 6)
+time.sleep(wait_time)
+start_time = my_timer()
+input('Press ENTER to stop')
+end_time = my_timer()
+
+print('Starterd at: ' + time.strftime("%X", time.localtime(start_time)))
+print('Stopped at: ' + time.strftime("%X", time.localtime(end_time)))
+print(f'Your reaction time was {end_time - start_time} seconds')
