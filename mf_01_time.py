fbbd5290f61eea010f4de6bacceaee1e247367bd
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-17 22:51:46 +0700

    comment on ways to print time

--- mf_01_time.py
+++ mf_01_time.py
@@ -8,6 +8,6 @@ import time
 
 time_here = time.localtime()
 print(time_here)
-print('Year: ', time_here[0], time_here.tm_year)    # two ways to, code is equal
-print('Month: ', time_here[1], time_here.tm_mon)
-print('Day: ', time_here[2], time_here.tm_mday)     # mday!
+print('Year: ', time_here[0], time_here.tm_year)    # two ways to, code is equal - no practical diff
+print('Month: ', time_here[1], time_here.tm_mon)    # second way is easier to read
+print('Day: ', time_here[2], time_here.tm_mday)
