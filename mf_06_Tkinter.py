30aa55bcf5ea8428f54fbe01bde0f5c6d34b115a
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-20 16:07:01 +0700

    windows offset using =/-

--- mf_06_Tkinter.py
+++ mf_06_Tkinter.py
@@ -5,5 +5,5 @@ print(tkinter.TkVersion)
 # tkinter._test()
 mainWindow = tkinter.Tk()
 mainWindow.title("My frirst Window")
-mainWindow.geometry('640x480')
+mainWindow.geometry('640x480-1000-1000') # add +/- to set offset of window
 mainWindow.mainloop()
