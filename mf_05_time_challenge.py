cdf1807a4aacc78aaad327a870a21771b992c746
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-20 12:14:31 +0700

    change dictionary keys to be str not int

--- mf_05_time_challenge.py
+++ mf_05_time_challenge.py
@@ -14,36 +14,24 @@ import datetime
 import pytz
 
 # 0: Quit
-zones = {1: 'Africa/Addis_Ababa',
-         2: 'America/Argentina/Buenos_Aires',
-         3: 'America/Barbados',
-         4: 'America/La_Paz',
-         5: 'Antarctica/Troll',
-         6: 'Asia/Bangkok',
-         7: 'Asia/Beirut',
-         8: 'Asia/Jakarta',
-         9: 'Asia/Phnom_Penh',
+zones = {'1': 'Africa/Addis_Ababa',
+         '2': 'America/Argentina/Buenos_Aires',
+         '3': 'America/Barbados',
+         '4': 'America/La_Paz',
+         '5': 'Antarctica/Troll',
+         '6': 'Asia/Bangkok',
+         '7': 'Asia/Beirut',
+         '8': 'Asia/Jakarta',
+         '9': 'Asia/Phnom_Penh',
          }
 
-# choice = 2
-# n_zone = pytz.timezone(zones[choice])
-# print(n_zone)
-# time_in_n = datetime.datetime.now(tz=n_zone)
-# print(time_in_n)
-# local_time = datetime.datetime.now
-# utc_time = datetime.datetime.utcnow()
-#
-# print(f'You selected {choice} : \n '
-#       f'time zone : {n_zone} : current date and time : {time_in_n} \n'
-#       f'Your local time is: {local_time} \n'
-#       f'and UTC time is: {utc_time}')
-
+print(type(zones))
 choice = None
 while True:
     if choice == 0:
         break
 
-    if choice in range(10):
+    if choice in zones.keys():
         n_zone = pytz.timezone(zones[choice])
         print(n_zone)
         time_in_n = datetime.datetime.now(tz=n_zone)
@@ -62,4 +50,4 @@ while True:
         print('0: For Exit')
         for number, name in zones.items():
             print(f'{number} : {name}')
-        choice = int(input())
+        choice = input()
