25269eaca827c5ad2d1fe4eb244bb9bea4926fad
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-20 16:01:53 +0700

    tkinter - create first window

---
+++ mf_06_Tkinter.py
@@ -0,0 +1,9 @@
+import tkinter
+
+print(tkinter.TkVersion)
+
+# tkinter._test()
+mainWindow = tkinter.Tk()
+mainWindow.title("My frirst Window")
+mainWindow.geometry('640x480')
+mainWindow.mainloop()
