8ba70b3b725f60da4aae928410052d4584c48847
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-22 21:56:08 +0700

    def function to draw x y

--- mf_09_parabola.py
+++ mf_09_parabola.py
@@ -1,36 +1,47 @@
 import tkinter
 
-def parabola(x: int) -> int:
-    """Returns y equals x square
 
-    :param x: any int
+def parabola(numb: int) -> int:
+    """
+    Returns y equals x square
+    :param numb: any int
     :return: y which is x square
     """
-    y = x * x
-    return y
+    numb_square = numb * numb / 50
+    return numb_square
 
 
 def draw_axes(canvas) -> None:
+    """
+    Draw axes x y
+    :param canvas:
+    :return:
+    """
     canvas.update()
     x_origin = canvas.winfo_width() / 2
     y_origin = canvas.winfo_height() / 2
     canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
-    canvas.create_line(-x_origin, 0, x_origin, 0, fill="red")
-    canvas.create_line(0, -y_origin, 0, y_origin, fill="red")
+    canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    canvas.create_line(0, -y_origin, 0, y_origin, fill="black")
+
+
+def draw_plot(canvas, x: int, y: int) -> None:
+    canvas.create_line(x, y, x + 1, y + 1, fill="red")
 
 
 mainWindow = tkinter.Tk()
 mainWindow.title("Parabola")
 mainWindow.geometry('800x600')
 
-canvas = tkinter.Canvas(mainWindow, width=800, height=600)
-canvas.grid(row=0, column=0)
+canvas1 = tkinter.Canvas(mainWindow, width=800, height=600)
+canvas1.grid(row=0, column=0)
 
-draw_axes(canvas)
+draw_axes(canvas1)
+
 
 for x in range(-100, 100):
     y = parabola(x)
-    print(y)
+    draw_plot(canvas1, x, -y)
 
 
 
