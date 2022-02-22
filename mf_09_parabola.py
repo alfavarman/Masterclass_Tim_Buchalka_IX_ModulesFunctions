e8b935f9535ea1ffed7f32bfff001e10c531bc59
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-22 19:02:28 +0700

    def function to draw axes

--- mf_09_parabola.py
+++ mf_09_parabola.py
@@ -1,4 +1,6 @@
-def parabola(x: int) -> y:
+import tkinter
+
+def parabola(x: int) -> int:
     """Returns y equals x square
 
     :param x: any int
@@ -7,3 +9,30 @@ def parabola(x: int) -> y:
     y = x * x
     return y
 
+
+def draw_axes(canvas) -> None:
+    canvas.update()
+    x_origin = canvas.winfo_width() / 2
+    y_origin = canvas.winfo_height() / 2
+    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    canvas.create_line(-x_origin, 0, x_origin, 0, fill="red")
+    canvas.create_line(0, -y_origin, 0, y_origin, fill="red")
+
+
+mainWindow = tkinter.Tk()
+mainWindow.title("Parabola")
+mainWindow.geometry('800x600')
+
+canvas = tkinter.Canvas(mainWindow, width=800, height=600)
+canvas.grid(row=0, column=0)
+
+draw_axes(canvas)
+
+for x in range(-100, 100):
+    y = parabola(x)
+    print(y)
+
+
+
+
+mainWindow.mainloop()
