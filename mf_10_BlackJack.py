cfa29110fb8a70789442f4367e458a5940fb7e29
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-23 14:33:45 +0700

    Black Jack: write window, frame for table, dealer player, buttons and cards. load cards png def to load cards

---
+++ mf_10_BlackJack.py
@@ -0,0 +1,93 @@
+import tkinter, random
+
+
+def load_cards(card_images):
+    suits = ['heart', 'club', 'diamond', 'spade']
+    face_cards = ['jack', 'queen', 'king']
+
+    # # Tkinter ver below 8.6 doesnt read png
+    # if tkinter.TkVersion >= 8.6
+    #     extension = 'png'
+    # else:
+    #     extension = 'ppm'
+
+    # append suites to card_images
+    for suit in suits:
+        for card in range(1, 11):
+            # card_n = f'{card:02d}' IF file name has 0 in front
+            name = f'cards_png/{str(card)}_{suit}.png'
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((card, image, name))
+
+        for card in face_cards:
+            name = f'cards_png/{str(card)}_{suit}.png'
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((card, image, name))
+
+
+# open and set up window
+mainWindow = tkinter.Tk()
+mainWindow.title('BlackJack')
+mainWindow.geometry('800x600')
+
+# Text -> Variable 'May the best win!' -> 'Dealer/Player Wins! Congratulations!'
+# result_text = tkinter.StringVar
+labelResult = tkinter.Label(mainWindow, text="May the best Win!", fg='blue')
+labelResult.grid(row=0, column=0, columnspan=6)
+
+# frame for table
+frameTable = tkinter.Frame(mainWindow, relief='sunken', background='green')
+frameTable.grid(row=1, column=0, rowspan=2, columnspan=5, sticky='nsew')
+
+# Dealer'
+# frame for dealer label and points
+frameDealer = tkinter.Frame(frameTable, background='green')
+frameDealer.grid(row=0, column=0)
+
+# frame for dealer_cards
+frameDealer_cards = tkinter.Frame(frameTable, background='green')
+frameDealer_cards.grid(row=0, column=1, columnspan=4)
+
+# label for dealer
+labelDealer = tkinter.Label(frameDealer, text='Dealer', fg='white', background='green')
+labelDealer.grid(row=0, column=0, sticky='new')
+
+# label for dealer points
+labelDealer_Points = tkinter.IntVar()
+tkinter.Label(frameDealer, text='Dealer', fg='white', background='green').grid(row=0, column=0)
+tkinter.Label(frameDealer, textvariable=labelDealer_Points, fg='white', background='green').grid(row=1, column=0)
+
+# Player'
+# frame for player label and points
+framePlayer = tkinter.Frame(frameTable, background='green')
+framePlayer.grid(row=1, column=0)
+
+# frame for player_cards
+framePlayer_cards = tkinter.Frame(frameTable, background='green')
+framePlayer_cards.grid(row=1, column=1, columnspan=4, sticky='we')
+
+# label for player
+labelPlayer = tkinter.Label(framePlayer, text='Player', fg='white', background='green')
+labelPlayer.grid(row=0, column=0, sticky='new')
+
+# label for player points
+labelPlayer_Points = tkinter.IntVar()
+tkinter.Label(framePlayer, text='Player', fg='white', background='green').grid(row=0, column=0)
+tkinter.Label(framePlayer, textvariable=labelPlayer_Points, fg='white', background='green').grid(row=1, column=0)
+
+# frame for buttons:
+frameButtons = tkinter.Frame(mainWindow)
+frameButtons.grid(row=3, column=0)
+
+# Dealer Button
+buttonDealer = tkinter.Button(frameButtons, text='Dealer', relief='raised', borderwidth=2)
+buttonDealer.grid(row=0, column=0)
+# Player Button
+buttonPlayer = tkinter.Button(frameButtons, text='Player', relief='raised', borderwidth=2)
+buttonPlayer.grid(row=0, column=1)
+
+card = []
+load_cards(card)
+print(card)
+
+mainWindow.mainloop()
